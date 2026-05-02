97f7dada1c898944ce96f847423d61d361ad7034
chris <chris@13bf7ae8-c1e8-40eb-9c39-9d3cd1688aa4.fritz.box>
2026-05-02 23:19:02 +0200

    add login system with basic validation

---
+++ js/login.js
@@ -0,0 +1,26 @@
+console.log("Login logik geladen...");
+// Holen der Id'S
+let usernameInput = document.getElementById("usernameInput");
+let passwordInput = document.getElementById("passwordInput");
+let rememberMe = document.getElementById("rememberMe");
+let loginBtn = document.getElementById("loginBtn");
+let loginMessage = document.getElementById("loginMessage");
+
+// Test loginBtn klick
+loginBtn.addEventListener("click", function() {
+   console.log(usernameInput.value);
+   console.log(passwordInput.value);
+
+   // Check ob leere eingaben getätigt werden
+   if ( usernameInput.value === "NomicGDev" && passwordInput.value === "999" ) {
+    loginMessage.style.color = "LimeGreen";
+    loginMessage.textContent = " Login Erfolgreich!"
+   } else if ( usernameInput.value === "" || passwordInput.value === "" ) {
+    loginMessage.textContent = "Bitte alles ausfüllen!";
+    loginMessage.style.color = "orange";
+   } else {
+    loginMessage.textContent = "Login fehlgeschlagen";
+    loginMessage.style.color = "red";
+   }
+})
+
